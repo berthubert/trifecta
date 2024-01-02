77acd6b3d468ef661307f50584d35fedbd15808f
bert hubert <bert@hubertnet.nl>
2024-01-02 13:58:24 +0100

    ascii rename

--- support.cc
+++ support.cc
@@ -1,9 +1,10 @@
 #include <iostream>
+
 #include <regex>
-
 #include "cryptopp/base64.h"
-
 #include "support.hh"
+#include <sclasses.hh>
+#include <fmt/chrono.h>
 
 using namespace std;
 // turn "abcd=1234; defgh=6934"3 into a map
@@ -44,3 +45,60 @@ string makeShortID(int64_t id)
                   ); // StringSource
   return encoded;
 }
+
+// do not put UTF-8 or anything in this, it won't work. US-ASCII. 
+void sendAsciiEmailAsync(const std::string& from, const std::string& to, const std::string& subject, const std::string& textBody)
+{
+  ComboAddress mailserver("10.0.0.2:25");
+  Socket s(mailserver.sin4.sin_family, SOCK_STREAM);
+
+  SocketCommunicator sc(s);
+  sc.connect(mailserver);
+  string line;
+  auto sponge= [&](int expected) {
+    while(sc.getLine(line)) {
+      if(line.size() < 4)
+        throw std::runtime_error("Invalid response from SMTP server: '"+line+"'");
+      if(stoi(line.substr(0,3)) != expected)
+        throw std::runtime_error("Unexpected response from SMTP server: '"+line+"'");
+      if(line.at(3) == ' ')
+        break;
+    }
+  };
+
+  sponge(220);
+  sc.writen("EHLO dan\r\n");
+  sponge(250);
+
+  sc.writen("MAIL From:<"+from+">\r\n");
+  sponge(250);
+
+  sc.writen("RCPT To:<"+to+">\r\n");
+  sponge(250);
+
+  sc.writen("DATA\r\n");
+  sponge(354);
+  sc.writen("From: "+from+"\r\n");
+  sc.writen("To: "+to+"\r\n");
+  sc.writen("Subject: "+subject+"\r\n");
+
+  //Date: Thu, 28 Dec 2023 14:31:37 +0100 (CET)
+  sc.writen(fmt::format("Date: {:%a, %d %b %Y %H:%M:%S %z (%Z)}\r\n", fmt::localtime(time(0))));
+  sc.writen("\r\n");
+
+
+  string withCrlf;
+  for(auto iter = textBody.cbegin(); iter != textBody.cend(); ++iter) {
+    if(*iter=='\n' && (iter == textBody.cbegin() || *std::prev(iter)!='\r'))
+      withCrlf.append(1, '\r');
+    if(*iter=='.' && (iter != textBody.cbegin() && *std::prev(iter)=='\n'))
+      withCrlf.append(1, '.');
+        
+    withCrlf.append(1, *iter);
+  }
+  
+  sc.writen(withCrlf);
+  sc.writen("\r\n.\r\n");
+  sponge(250);
+}
+
