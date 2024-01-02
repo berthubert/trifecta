bc6f1f914b46e3ba074da19daa4ff4da7d0894bf
bert hubert <bert@hubertnet.nl>
2024-01-02 13:58:04 +0100

    ascii rename

--- support.hh
+++ support.hh
@@ -2,8 +2,10 @@
 #include <unordered_map>
 #include <string>
 
+
 int trifectaMain(int argc, const char* argv[]);
 std::unordered_map<std::string, std::string> getCookies(const std::string& cookiestr);
 std::string makeShortID(int64_t id);
 
 std::string& testrunnerPw();
+void sendAsciiEmailAsync(const std::string& from, const std::string& to, const std::string& subject, const std::string& textBody);
