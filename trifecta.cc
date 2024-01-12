5503df91a263e718e87813a181b38abfc709e9a2
bert hubert <bert@hubertnet.nl>
2024-01-12 17:01:50 +0100

    improve error reporting on commandline on exception

--- trifecta.cc
+++ trifecta.cc
@@ -1,6 +1,11 @@
 #include "support.hh"
-
+using namespace std;
 int main(int argc, char* argv[])
+try
 {
   return trifectaMain(argc, (const char**)argv);
+}
+catch(std::exception& e) {
+  cerr<<"Error: "<<e.what()<<endl;
+  return EXIT_FAILURE;
 }
