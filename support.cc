b72f280d076b1bc4b588d5c7f3227c1324ee14e5
bert hubert <bert@hubertnet.nl>
2024-01-27 13:09:29 +0100

    If you'd send us megabytes of cookies we'd spend a lot of time parsing them. Thank you Wander Nauta, closes #45

--- support.cc
+++ support.cc
@@ -35,12 +35,14 @@ string htmlEscape(const std::string& str)
 // turn "abcd=1234; defgh=6934" into a map
 static unordered_map<string,string> getGen(const std::string& cookiestr, const string& sep)
 {
+  unordered_map<string,string> ret;
+  if(cookiestr.size() > 2000) // this regex could take quite some time otherwise, thanks Wander Nauta
+    return ret;
   std::regex cookie_regex("([^=]*=[^"+sep.substr(0,1)+"]*)");
   auto cookies_begin =
     std::sregex_iterator(cookiestr.begin(), cookiestr.end(), cookie_regex);
   auto cookies_end = std::sregex_iterator();
 
-  unordered_map<string,string> ret;
   for(auto iter = cookies_begin; iter != cookies_end; ++iter) {
     std::regex inner("("+sep+")?([^=]*)=([^"+sep.substr(0,1)+"]*)");
     std::smatch m;
